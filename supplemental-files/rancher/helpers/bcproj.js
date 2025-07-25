a7a673b6e81cbce4baabc0a887a73dbcccc59cb2
John Krug <john.krug@suse.com>
2025-07-25 17:16:26 +0100

    Further refinement.
    Signed-off-by: John Krug <john.krug@suse.com>

--- supplemental-files/rancher/helpers/bcproj.js
+++ supplemental-files/rancher/helpers/bcproj.js
@@ -4,17 +4,19 @@ const get_project_code = (url) => url.split('/')[1] || null
 
 module.exports = (request, nav, projectData) => {
   if (nav.page.layout == '404') return null;
+
   const project_code = get_project_code(nav.page.url);
-  var ret = null;
+  if (!project_code) return null;
+
+  const project = projectData.find(obj => obj['url-part'] === project_code);
+  if (!project) return null;
+
   switch(request) {
     case 'title':
-      ret = projectData.find(obj => obj['url-part'] === project_code)?.title || null;
-      break;
+      return project.title || null;
     case 'url':
-      ret = projectData.find(obj => obj['url-part'] === project_code)?.url || null;
-      break;
+      return project.url || null;
     default:
-      ret = null;
+      return null;
   }
-  return ret;
 }
